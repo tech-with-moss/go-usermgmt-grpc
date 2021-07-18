8678c18381f526c48bd6d6f2c253d86333088f2a
Moss <83650740+tech-with-moss@users.noreply.github.com>
2021-07-18 18:21:12 -0400

    working on writing JSON to file and back to a protobuf message

--- usermgmt_server/usermgmt_server.go
+++ usermgmt_server/usermgmt_server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
+	"os"
 
 	pb "github.com/tech-with-moss/go-usermgmt-grpc/usermgmt"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 const (
@@ -39,14 +43,46 @@ func (server *UserManagementServer) Run() error {
 
 func (server *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
+
+	readBytes, err := ioutil.ReadFile("../users.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("%s: File not found.  Creating new file.\n", "../users.json")
+		} else {
+			log.Fatalln("Error reading file:", err)
+		}
+	}
+
+	var users_list *pb.UsersList = &pb.UsersList{}
+
+	if err := protojson.Unmarshal(readBytes, users_list); err != nil {
+		log.Fatalf("Failed to parse user list: %v", err)
+	}
+
 	var user_id = int32(rand.Intn(100))
 	created_user := &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}
-	server.users_list.Users = append(server.users_list.Users, created_user)
+	users_list.Users = append(users_list.Users, created_user)
+	jsonBytes, err := protojson.Marshal(users_list)
+	if err != nil {
+		log.Fatalf("JSON Marshaling failed: %v", err)
+	}
+	if err := ioutil.WriteFile("../users.json", jsonBytes, 0664); err != nil {
+		log.Fatalf("Failed write to file: %v", err)
+	}
 	return created_user, nil
 }
 
 func (server *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UsersList, error) {
-	return server.users_list, nil
+	jsonBytes, err := ioutil.ReadFile("../users.json")
+	if err != nil {
+		log.Fatalf("Failed read from file: %v", err)
+	}
+	var users_list *pb.UsersList = &pb.UsersList{}
+	if err := protojson.Unmarshal(jsonBytes, users_list); err != nil {
+		log.Fatalf("Unmarshaling failed: %v", err)
+	}
+
+	return users_list, nil
 }
 
 func main() {
