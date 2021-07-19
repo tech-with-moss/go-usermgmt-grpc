3f620c2615656e519a6de59caef6b295a236e8b1
Moss <83650740+tech-with-moss@users.noreply.github.com>
2021-07-18 23:11:35 -0400

    marshaling and unmarshaling of JSON data works correctly

--- usermgmt_server/usermgmt_server.go
+++ usermgmt_server/usermgmt_server.go
@@ -19,14 +19,11 @@ const (
 )
 
 func NewUserManagementServer() *UserManagementServer {
-	return &UserManagementServer{
-		users_list: &pb.UsersList{},
-	}
+	return &UserManagementServer{}
 }
 
 type UserManagementServer struct {
 	pb.UnimplementedUserManagementServer
-	users_list *pb.UsersList
 }
 
 func (server *UserManagementServer) Run() error {
@@ -41,26 +38,36 @@ func (server *UserManagementServer) Run() error {
 	return s.Serve(lis)
 }
 
+//When user is added, read full userlist from file into
+//userlist struct, then append new user and write new userlist back to file
 func (server *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
-	log.Printf("Received: %v", in.GetName())
 
+	log.Printf("Received: %v", in.GetName())
 	readBytes, err := ioutil.ReadFile("../users.json")
+	var users_list *pb.UsersList = &pb.UsersList{}
+	var user_id = int32(rand.Intn(100))
+	created_user := &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}
+
 	if err != nil {
 		if os.IsNotExist(err) {
 			fmt.Printf("%s: File not found.  Creating new file.\n", "../users.json")
+			users_list.Users = append(users_list.Users, created_user)
+			jsonBytes, err := protojson.Marshal(users_list)
+			if err != nil {
+				log.Fatalf("JSON Marshaling failed: %v", err)
+			}
+			if err := ioutil.WriteFile("../users.json", jsonBytes, 0664); err != nil {
+				log.Fatalf("Failed write to file: %v", err)
+			}
+			return created_user, nil
 		} else {
 			log.Fatalln("Error reading file:", err)
 		}
 	}
 
-	var users_list *pb.UsersList = &pb.UsersList{}
-
 	if err := protojson.Unmarshal(readBytes, users_list); err != nil {
 		log.Fatalf("Failed to parse user list: %v", err)
 	}
-
-	var user_id = int32(rand.Intn(100))
-	created_user := &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}
 	users_list.Users = append(users_list.Users, created_user)
 	jsonBytes, err := protojson.Marshal(users_list)
 	if err != nil {
@@ -70,6 +77,7 @@ func (server *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.Ne
 		log.Fatalf("Failed write to file: %v", err)
 	}
 	return created_user, nil
+
 }
 
 func (server *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UsersList, error) {
